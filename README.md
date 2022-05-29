5a769e23702ba26b168cb9f2db968ffc0b293eab
timofeji <contact@timofejjermolaev.com>
2022-05-29 19:19:31 -0400

    added glad

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-#setup
+<img src="https://user-images.githubusercontent.com/11130291/168183021-e40cf3c5-4648-40f3-9d79-37c6a404aa1a.png" alt="drawing" width="200"/>
+Sim-kit is a small rendering/simulations library written in C++ cross compiling for desktop and web using emscripten.
+
+##### Setup
+##### Windows:
+```C
+    ./setup.bat
+```
+
+## how to use
+##### 1.) Install package
--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,36 @@
+#define GLEW_STATIC
+#include <glad/glad.h>
 #include <SDL.h>
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        exit(1);
+    }
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-
-    SDL_Window* window = SDL_CreateWindow("Sim-Kit", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("Sim-Kit", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
     SDL_Event windowEvent;
+
+    gladLoadGLES2Loader((GLADloadproc)SDL_GL_GetProcAddress);
+
     while (true)
     {
+        glViewport(0, 0, 800, 600);
+
         if (SDL_PollEvent(&windowEvent))
         {
             if (windowEvent.type == SDL_QUIT)
                 break;
         }
-
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(window);
     }
 
